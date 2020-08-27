68c1722180435d237fa88ada4f90f5a33624c12b
Sai Krishnam raju <saikrishnamraju384@gmail.com>
2020-08-27 22:44:53 +0530

    Updates

---
+++ precache-manifest.d7d0e3acf6c9f5b4ab24f1105fb408bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b54aa743c823e699d680ec6bd6882c0d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b8c12c45a13aee446cab",
+    "url": "/monsters-rolodex/static/css/main.30269b52.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8c12c45a13aee446cab",
+    "url": "/monsters-rolodex/static/js/main.ca7f25aa.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
